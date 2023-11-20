9377c4cb8de6c8e7693ad4189016ce55359c603e
Go Shibata <g_shibata@yumemi.co.jp>
2023-11-20 18:41:12 +0900

    最初に表示する画面を SplashScreen に変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/main_screen.dart';
+import 'package:flutter_training/splash_screen.dart';
 
 void main() {
-  runApp(const MainScreen());
+  runApp(
+    MaterialApp(
+      initialRoute: '/splash',
+      routes: <String, WidgetBuilder>{
+        '/main': (_) => const MainScreen(),
+        '/splash': (_) => const SplashScreen(),
+      },
+    ),
+  );
 }
