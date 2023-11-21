dc252ae0621ad3cc1bad069b7c5e568bb4fc6824
Go Shibata <g_shibata@yumemi.co.jp>
2023-11-21 17:46:56 +0900

    0.5 秒後に遷移する処理の実装

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,6 +11,32 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  static const _waitInMillis = 500;
+
+  @override
+  void initState() {
+    super.initState();
+    unawaited(_transition(context));
+  }
+
+  Future<void> _transition(BuildContext context) async {
+    await SchedulerBinding.instance.endOfFrame;
+    await Future<void>.delayed(
+      const Duration(milliseconds: _waitInMillis),
+    );
+
+    if (!context.mounted) {
+      return;
+    }
+    // 遷移先から戻るまで待機
+    await Navigator.pushNamed(context, '/main');
+
+    if (!context.mounted) {
+      return;
+    }
+    unawaited(_transition(context));
+  }
+
   @override
   Widget build(BuildContext context) {
     return const DecoratedBox(
