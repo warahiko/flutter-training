b7a861ef3b0cfe25f9d48969e1e540d9fb03a242
Go Shibata <g_shibata@yumemi.co.jp>
2023-11-22 16:40:51 +0900

    Forecast の追加

---
+++ lib/forecast.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter_training/weather.dart';
+
+class Forecast {
+  const Forecast({
+    required this.weather,
+    required this.maxTemperature,
+    required this.minTemperature,
+  });
+
+  factory Forecast.from(String jsonString) {
+    final decoded = jsonDecode(jsonString);
+
+    return Forecast(
+      weather: Weather.from(decoded['weather_condition'] as String),
+      minTemperature: decoded['min_temperature'] as int,
+      maxTemperature: decoded['max_temperature'] as int,
+    );
+  }
+
+  final Weather weather;
+  final int maxTemperature;
+  final int minTemperature;
+}
