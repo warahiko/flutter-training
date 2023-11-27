ee98dd41e01d24cdbe16d89c21be9d9e30d091b5
Go Shibata <g_shibata@yumemi.co.jp>
2023-11-27 18:51:58 +0900

    main_screen.dart を ui/main ディレクトリに移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/main_screen.dart';
+import 'package:flutter_training/ui/main/main_screen.dart';
 import 'package:flutter_training/ui/screen.dart';
 import 'package:flutter_training/ui/splash/splash_screen.dart';
 
