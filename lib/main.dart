ae3e6c149f451daa993f5f910a7ee4b8653e6a6f
Go Shibata <g_shibata@yumemi.co.jp>
2023-11-28 18:16:09 +0900

    riverpod が使えるように、runApp の直下に ProviderScope を配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_training/ui/main/main_screen.dart';
 import 'package:flutter_training/ui/screen.dart';
 import 'package:flutter_training/ui/splash/splash_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      initialRoute: Screen.splash.route,
-      routes: <String, WidgetBuilder>{
-        Screen.main.route: (_) => const MainScreen(),
-        Screen.splash.route: (_) => const SplashScreen(),
-      },
+    ProviderScope(
+      child: MaterialApp(
+        initialRoute: Screen.splash.route,
+        routes: <String, WidgetBuilder>{
+          Screen.main.route: (_) => const MainScreen(),
+          Screen.splash.route: (_) => const SplashScreen(),
+        },
+      ),
     ),
   );
 }
